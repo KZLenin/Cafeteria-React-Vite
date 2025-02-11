bbad2cf4be3d54abcf2dbd4814f60972c57a4b61
KZLenin <leninmateo4@gmail.com>
2025-02-11 00:40:13 -0500

    React incorporated

--- src/components/Inicio/DescargaApp.jsx
+++ src/components/Inicio/DescargaApp.jsx
@@ -2,40 +2,47 @@ import React from "react";
 
 const DescargaApp = () => {
   return (
-    <section className="descarga-app">
-      <h1 className="title-descarga">Descarga nuestra app!</h1>
-      <p className="descarga-description">
+    <section className="container text-center my-5">
+      <h1 className="fw-bold">¡Descarga nuestra app!</h1>
+      <p className="lead">
         Reserva una mesa o un espacio para ti directamente desde nuestra app móvil.
       </p>
-      <img 
-        className='app-img' 
-        src="https://i.ytimg.com/vi/lySRx75rsoo/maxresdefault.jpg" 
-        alt="Vista previa de la aplicación" 
-      />
       
+      {/* Imagen de la aplicación */}
+      <div className="d-flex justify-content-center my-4">
+        <img
+          className="img-fluid rounded shadow"
+          src="https://i.ytimg.com/vi/lySRx75rsoo/maxresdefault.jpg"
+          alt="Vista previa de la aplicación"
+          style={{ maxWidth: "600px" }}
+        />
+      </div>
+
       {/* Botones de descarga */}
-      <div className="descarga-botones">
-        <a 
-          href="https://www.apple.com/es/app-store/" 
-          target="_blank" 
+      <div className="d-flex justify-content-center gap-3 mt-4">
+        <a
+          href="https://www.apple.com/es/app-store/"
+          target="_blank"
           rel="noopener noreferrer"
         >
-          <img 
-            src="https://developer.apple.com/app-store/marketing/guidelines/images/badge-download-on-the-app-store.svg" 
-            alt="Descargar en App Store" 
-            className="app-store-btn"
+          <img
+            src="https://developer.apple.com/app-store/marketing/guidelines/images/badge-download-on-the-app-store.svg"
+            alt="Descargar en App Store"
+            className="img-fluid"
+            style={{ maxWidth: "150px" }}
           />
         </a>
-        
-        <a 
-          href="https://play.google.com/store/games?device=windows" 
-          target="_blank" 
+
+        <a
+          href="https://play.google.com/store/games?device=windows"
+          target="_blank"
           rel="noopener noreferrer"
         >
-          <img 
-            src="https://olelifego.com/wp-content/uploads/2022/09/googeplay-1.png" 
-            alt="Disponible en Google Play" 
-            className="google-play-btn"
+          <img
+            src="https://olelifego.com/wp-content/uploads/2022/09/googeplay-1.png"
+            alt="Disponible en Google Play"
+            className="img-fluid"
+            style={{ maxWidth: "170px" }}
           />
         </a>
       </div>
