ca2867257dca24e8cbfd597c8e595dcbcb679c6f
KZLenin <leninmateo4@gmail.com>
2025-02-10 21:42:27 -0500

    Login Button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
+import appFirebase from "../credenciales";
 import "../styles/header.css";
 import Logo from "../assets/Held Coffee Shop Logo.png";
 
+const auth = getAuth(appFirebase);
+
 const Header = () => {
+  const [usuario, setUsuario] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUsuario(user);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const handleAuth = () => {
+    if (usuario) {
+      signOut(auth);
+    } else {
+      window.location.href = "/login"; // Redirige a la página de login
+    }
+  };
   return (
     <header className="header">
       <div className="menu container">
@@ -15,6 +35,8 @@ const Header = () => {
             <li><a href="/informacion">Información</a></li>
             <li><a href="/menu">Menú</a></li>
             <li><a href="/reservas">Reservas</a></li>
+            <button type="button" class="btn btn-outline-light btn-sm" onClick={handleAuth}>
+            {usuario ? "Cerrar sesión" : "Login"}</button>
           </ul>
         </nav>
       </div>
