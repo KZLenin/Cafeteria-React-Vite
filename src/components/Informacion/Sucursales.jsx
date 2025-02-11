ff4878d7cb29ebe2af3e7815b690b75678be4b5b
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2025-02-11 09:10:52 -0500

    txtsucursales

--- src/components/Informacion/Sucursales.jsx
+++ src/components/Informacion/Sucursales.jsx
@@ -4,19 +4,19 @@ const sucursalesData = [
   {
     nombre: "Sucursal Sur",
     direccion:
-      "Al sur nos encuentras en la Villaflora, Av. Rodrigo de Chávez y 5 de Junio, frente al Parque de las Flores.",
+      "Al sur, Av. Rodrigo de Chavez Y 5 de Junio.",
     mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7835392682196!2d-78.52567942634947!3d-0.2392394997584052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d5990adc06fca5%3A0x85e30a742a6a7d7c!2sRodrigo%20de%20Chavez%20Y%205%20de%20Junio!5e0!3m2!1ses!2sec!4v1731954961559!5m2!1ses!2sec",
   },
   {
-    nombre: "Sucursal Centro",
+    nombre: "Sucursal Centro",  
     direccion:
-      "En el centro nos encuentras en la Plaza Santo Domingo, Av. Eugenio Espejo y Juan Pío Montúfar.",
+      "Al centro, Mideros Oe6-46, Quito 170148.",
     mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1410.6034694425653!2d-78.51550359310141!3d-0.21922534566941357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d59a29f4ce75d1%3A0x72df29ec7cfd24be!2sCrepes%20Y%20Jugos%20Local%2014!5e0!3m2!1ses!2sec!4v1731955694541!5m2!1ses!2sec",
   },
   {
     nombre: "Sucursal Norte",
     direccion:
-      "En el norte nos encuentras en la Plaza Artigas, Av. 12 de octubre y Av. Coruña.",
+      "Al norte, Ave 12 de Octubre, Quito 170109.",
     mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.793142337749!2d-78.48551272634954!3d-0.203550399794444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d59b00621dc977%3A0x20ae986207c07c62!2sPlaza%20Artigas!5e0!3m2!1ses!2sec!4v1739247128839!5m2!1ses!2sec",
   },
 ];
