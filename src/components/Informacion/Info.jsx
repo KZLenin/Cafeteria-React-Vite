4597da0e40119c5800711ccb4e8d406a39d318cb
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2025-02-11 09:03:24 -0500

    Fix info

--- src/components/Informacion/Info.jsx
+++ src/components/Informacion/Info.jsx
@@ -1,12 +1,14 @@
 const Info = () => {
     return(
         <section className="conocenos">
-        <h2>Conócenos</h2>
-        <p className="conocenos-p">
-        Somos una cafetería acogedora de ambiente cálido, ideal para que disfrutes de un buen café artesanal y postres caseros en compañía o en un momento de relax
-        </p>
+            <div className="container">
+                <h2>Conócenos</h2>
+                <p className="conocenos-p">
+                    Somos una cafetería acogedora de ambiente cálido, ideal para que disfrutes de un buen café artesanal y postres caseros en compañía o en un momento de relax.
+                </p>
+            </div>
         </section>
-
     );
 };
+
 export default Info;
